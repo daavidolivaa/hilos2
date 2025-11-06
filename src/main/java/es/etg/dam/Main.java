4ed36e303ccbb751348ed3eb13e57c667a703810
daavidolivaa <deividolivahuelamo.com>
2025-11-06 13:48:35 -0600

    2 commit

--- src/main/java/es/etg/dam/Main.java
+++ src/main/java/es/etg/dam/Main.java
@@ -1,16 +1,26 @@
 package es.etg.dam;
 
 public class Main {
+
+    public static final String MADRE1 = "Margarita";
+    public static final String MADRE2 = "Elena";
+    public static final String HIJO = "Manuel";
+    public static final String HIJA = "Maria";
+
+    public static final String DESPERTADOR = "Suena el despertador";
+
     public static void main(String[] args) {
 
-        Thread madre1 = new Thread(new Madre("Margarita"));
-        Thread madre2 = new Thread(new Madre("Elena"));
-        Thread hijo = new Thread(new Hijo("Manuel"));
-        Thread hija = new Thread(new HIja("Maria"));
+        Thread madre1 = new Thread(new Madre(MADRE1));
+        Thread madre2 = new Thread(new Madre(MADRE2));
+        Thread hijo = new Thread(new Hijo(HIJO));
+        Thread hija = new Thread(new HIja(HIJA));
 
-        madre1.start();
-        madre2.start();
-        hijo.start();
-        hija.start();
+        System.out.println(DESPERTADOR);
+
+        madre1.run();
+        madre2.run();
+        hijo.run();
+        hija.run();
     }
 }
--- src/main/java/es/etg/dam/Personas.java
+++ src/main/java/es/etg/dam/Personas.java
@@ -5,15 +5,18 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 abstract class Personas implements Runnable {
 
+    public static final String LEVANTA = "Se levanta";
+    public static final String METODO_RUN = "Unimplemented method 'run'";
+
     protected String nombre;
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        throw new UnsupportedOperationException(METODO_RUN);
     }
 
     public void levantarse() {
-        System.out.println("Se levanta " + nombre);
+        System.out.println(LEVANTA + nombre);
     }
 
 }
